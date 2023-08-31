515e768a3b242854dfb2f95c084fcedcc1649e81
Aarti Kumari <aartikumari31102003@gmail.com>
2023-08-31 18:01:57 +0530

    create firt website using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      'ms': {'max':'900px'},
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
